94284dccb8747b072674ef985262a84cfc8b52a1
Jens Broekens <jens.broekens@protonmail.com>
2024-03-12 20:55:01 +0100

    initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,68 @@
+plugins {
+	java
+}
+
+repositories {
+	mavenCentral()
+	maven("https://repo.papermc.io/repository/maven-public/")
+	maven("https://nexus.scarsz.me/content/groups/public/")
+}
+
+dependencies {
+	compileOnly(libs.paper.api)
+	compileOnly(libs.discordsrv.api)
+}
+
+val pat = "-"
+fun String.toCase(sep: String) = replace(pat, sep).lowercase()
+fun String.toClassName() = split(pat).joinToString("", transform={ it.lowercase().capitalize() })
+
+val pluginGroup: String by project
+val pluginName: String by project
+val pluginVersion: String by project
+val pluginDescription: String by project
+
+val pluginId = pluginName.toCase("-")
+
+group = pluginGroup
+version = pluginVersion
+description = pluginDescription
+base.archivesName = pluginId
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(libs.versions.java.get())
+	}
+}
+
+tasks {
+	processResources {
+		filesMatching("plugin.yml") {
+			expand(
+				"plugin" to mapOf(
+					"name" to pluginName,
+					"description" to pluginDescription,
+					"version" to pluginVersion,
+					"id" to pluginId,
+					"pkg" to "${pluginGroup}.${pluginName.toCase("_")}",
+					"class" to pluginName.toClassName(),
+				),
+				"versions" to mapOf(
+					"api" to libs.versions.paper.api.get().replace("\\.\\d+-.*".toRegex(), ""),
+				)
+			)
+		}
+	}
+
+	jar {
+		from("LICENSE") {
+			rename {
+				"LICENSE-${pluginId}"
+			}
+		}
+	}
+
+	withType<JavaCompile>().configureEach {
+		options.encoding = "UTF-8"
+	}
+}
---
+++ src/main/java/com/github/jenbroek/discordsrv_ignore_addon/DiscordsrvIgnoreAddon.java
@@ -0,0 +1,12 @@
+package com.github.jenbroek.discordsrv_ignore_addon;
+
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class DiscordsrvIgnoreAddon extends JavaPlugin implements Listener {
+
+	@Override
+	public void onEnable() {
+	}
+
+}
